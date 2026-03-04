607a36499b937e79ffba3cd92af11b078d5d8b47
OliverKayano <oliver12yoshio@hotmail.com>
2026-03-04 17:25:59 -0300

    Primeira versão

---
+++ SO_Lista01.04.py
@@ -0,0 +1,9 @@
+#Algoritmo SO_Lista01.04
+#Declarar.
+C: float = 0;
+F: float = 0;
+#Início.
+C = float(input("Inserir temperatura em C°:"));
+F = (((9*C)+160)/5);
+print("Temperatura em F:", F);
+#FIM.
